56432845e3bcb9f4bec86a6b85aceb1e7024d4c3
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-08-28 10:05:46 +0700

    Refactor views to classes. Added `end_date` Added `is_published` method Added `can_vote` method Added flake8 compliant comments.

--- polls/tests.py
+++ polls/tests.py
@@ -1,18 +1,27 @@
+"""Contain tests."""
 from json import dumps, loads
 from django.test import TestCase
-
-# to store session cookie
-session = None
+from polls.models import User
 
 
 class APITest(TestCase):
-    # Bro quit rolling back my test database.
+    """Implement tests for gyatt."""
+
+    # to store session cookie
+    session = None
+
     @classmethod
     def _rollback_atomics(cls, atomics):
+        """Override the rollback function.
+
+        To stop the rollback of the database for every test.
+        """
         return
 
     def test_a_list_empty(self):
-        """List empty, no polls are added yet.
+        """Get the list of polls.
+
+        The list should be empty, no polls are added yet.
         """
         response = self.client.post("/gyatt",
                                     data=dumps({"f": "list"}),
@@ -21,9 +30,7 @@ class APITest(TestCase):
         self.assertEqual(response.content, b'[]')
 
     def test_b_create_account(self):
-        """Create an account.
-        """
-        global session
+        """Create an account."""
         response = self.client.post("/gyatt",
                                     data=dumps({
                                         "f": "regis",
@@ -31,16 +38,33 @@ class APITest(TestCase):
                                         "p": "1234"
                                     }),
                                     content_type='application/json')
-        self.assertTrue(response.cookies["tk"] is not None,
-                        "`tk` cookie should be set.")
-        session = response.cookies
+        self.assertIsNotNone(response.cookies.get("tk"),
+                             "`tk` cookie should be set.")
+        self.assertIsNotNone(
+            User.objects.filter(username="User").first,
+            "User should be created.")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'ok')
+        self.__class__.session = response.cookies
 
-    def test_c_create_poll(self):
-        """Create a poll.
-        """
-        self.client.cookies = session
+    def test_c_create_poll_no_auth(self):
+        """Attempt to create a poll without authentication."""
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "create",
+                                        "n": "New Poll",
+                                        "i": "https://localhost:80/logo.png",
+                                        "a": 2,
+                                        "r": 2,
+                                        "b": 0,
+                                        "y": "yml_data here"
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+
+    def test_d_create_poll(self):
+        """Create a poll."""
+        self.client.cookies = self.__class__.session
         response = self.client.post("/gyatt",
                                     data=dumps({
                                         "f": "create",
@@ -54,9 +78,33 @@ class APITest(TestCase):
                                     content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
-    def test_d_list_one(self):
-        """List one poll.
-        """
+    def test_e_get_poll(self):
+        """Get a poll."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "get",
+                                        "n": "1",
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        data = loads(response.content)
+        self.assertEqual(data.get("yaml"), "yml_data here")
+
+    def test_f_not_already_answered(self):
+        """Check if the poll already is answered, should be False."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "aa",
+                                        "n": "1",
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"n")
+
+    def test_g_list_one(self):
+        """List one poll."""
         response = self.client.post("/gyatt",
                                     data=dumps({"f": "list"}),
                                     content_type='application/json')
@@ -66,11 +114,68 @@ class APITest(TestCase):
         self.assertEqual(data[0]["name"], "New Poll")
         self.assertEqual(data[0]["image"], "https://localhost:80/logo.png")
 
-    def test_e_invalid_rizz(self):
-        """Attempt to gyatt a fanum taxed rizz.
-        """
+    def test_f_submit(self):
+        """Submit the poll response."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "submit",
+                                        "n": "1",
+                                        "r": {
+                                            "test_key": "test_val"
+                                        }
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"ok")
+
+    def test_g_resubmit(self):
+        """Resubmit the poll response."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "submit",
+                                        "n": "1",
+                                        "r": {
+                                            "test_key2": "test_val2"
+                                        }
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"ok")
+
+    def test_h_res(self):
+        """Submit the poll response."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "res",
+                                        "n": "1"
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        data = loads(response.content)
+        self.assertIsNone(data.get("test_key"))
+        self.assertIsNotNone(data.get("test_key2"))
+        self.assertEqual(data.get("test_key2")[0].get("value"), "test_val2")
+        self.assertEqual(data.get("test_key2")[0].get("count"), 1)
+
+    def test_i_yes_already_answered(self):
+        """Check if the poll already is answered, should be False."""
+        self.client.cookies = self.__class__.session
+        response = self.client.post("/gyatt",
+                                    data=dumps({
+                                        "f": "aa",
+                                        "n": "1",
+                                    }),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"y")
+
+    def test_z_invalid_rizz(self):
+        """Attempt to gyatt a fanum taxed rizz."""
         response = self.client.post("/gyatt",
                                     data=dumps({"f": "DoesNotExist"}),
                                     content_type='application/json')
-        self.assertEqual(response.content, b"bad")
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content, b"Function not found")
+        self.assertEqual(response.status_code, 404)
--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,22 @@
+"""Contain urls."""
 from pathlib import Path
 from django.urls import re_path, path
 from django.conf.urls.static import static
-from .views import main, poll, rpc, create, auth, res
+from django.shortcuts import redirect
+from .views import RPCHandler, BasicView
 
-urlpatterns = static("/assets/", document_root=Path(__file__).parents[1].joinpath("frontend", "dist", "assets")) + [
-    path("polls", main),
-    path("polls/", main),
-    path("auth/", auth),
-    path("login", auth),
-    re_path("create/.*", create),
-    path("gyatt", rpc),
-    re_path("poll/.+", poll),
-    re_path("res/.+", res),
-    path("", main)
-]
+app_name = 'polls'
+
+urlpatterns = static(
+    "/assets/",
+    document_root=Path(__file__).parents[1].joinpath(
+        "frontend", "dist", "assets")) + [
+            path("gyatt", RPCHandler.as_view(), name="gyatt"),
+            path("polls", BasicView.as_view(), name="polls"),
+            path("auth", BasicView.as_view(), name="auth"),
+            path("login", lambda r: redirect("polls:auth")),
+            re_path("create/.*", BasicView.as_view(), name="create"),
+            re_path("poll/.+", BasicView.as_view(), name="poll"),
+            re_path("res/.+", BasicView.as_view(), name="res"),
+            path("", lambda r: redirect("polls:polls"))
+        ]
