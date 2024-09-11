aa923430b18a786f16e3c3a1a640af22064cce6e
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-09-11 12:32:06 +0700

    Force serving static files.

--- polls/urls.py
+++ polls/urls.py
@@ -1,27 +1,28 @@
 """Contain urls."""
 from pathlib import Path
-from django.urls import path
-from django.conf.urls.static import static
+from django.urls import path, re_path
+from django.views.static import serve
 from django.shortcuts import redirect
 from .views import RPCHandler, BasicView, RegisterView
 
 app_name = 'polls'
 
 urlpatterns = [
-        path("gyatt", RPCHandler.as_view(), name="gyatt"),
-        path("polls", BasicView.as_view(), name="polls"),
-        path("auth", lambda r: redirect("login")),
-        path("login", lambda r: redirect("login")),
-        path("admin", lambda r: redirect("polls:create")),
-        path("create/", BasicView.as_view(), name="create"),
-        path("register", RegisterView.as_view(), name="register"),
-        path("create/<int:poll_id>", BasicView.as_view(), name="create"),
-        path("poll/<int:poll_id>", BasicView.as_view(), name="poll"),
-        path("polls/<int:poll_id>",
-             lambda r, poll_id=None: redirect("polls:poll", poll_id=poll_id)),
-        path("res/<int:poll_id>", BasicView.as_view(), name="res"),
-        path("", lambda r: redirect("polls:polls"))
-    ] + static(
-    "/",
-    document_root=Path(__file__).parents[1].
-    joinpath("frontend", "dist"))
+    path("gyatt", RPCHandler.as_view(), name="gyatt"),
+    path("polls", BasicView.as_view(), name="polls"),
+    path("auth", lambda r: redirect("login")),
+    path("login", lambda r: redirect("login")),
+    path("admin", lambda r: redirect("polls:create")),
+    path("create/", BasicView.as_view(), name="create"),
+    path("register", RegisterView.as_view(), name="register"),
+    path("create/<int:poll_id>", BasicView.as_view(), name="create"),
+    path("poll/<int:poll_id>", BasicView.as_view(), name="poll"),
+    path("polls/<int:poll_id>",
+         lambda r, poll_id=None: redirect("polls:poll", poll_id=poll_id)),
+    path("res/<int:poll_id>", BasicView.as_view(), name="res"),
+    path("", lambda r: redirect("polls:polls")),
+    re_path("(?P<path>.*)$", serve, {
+        'document_root':
+        Path(__file__).parents[1].joinpath("frontend", "dist")
+    })
+]
